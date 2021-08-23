ce9c6089e192fbf01c1d8e35434543edfb43a133
mhb_dev <bayatim81@gmail.com>
2021-08-23 19:09:11 +0430

    with custom css and settings and cool front 8/23/2021

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import portfolio.views as views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('t/', views.t),
     path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,7 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects': projects})
+
+
+def t(request):
+    return render(request, 'portfolio/t.html')
